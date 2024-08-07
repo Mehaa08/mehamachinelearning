8f7effdf1823e46735851ca447852d6fa8842870
Mehaa08 <99003997+Mehaa08@users.noreply.github.com>
2024-08-07 22:40:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,10 @@ with st.expander('Data'):
   st.write('**Raw Data**')
   a = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   a
+  st.write('**x**')
+  x = a.drp('species',axis = 1)
+  a
+
+  st.write('**y**')
+  y = a.species
+  y
