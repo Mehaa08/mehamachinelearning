2a2aae0725128f06d4bf01e735d83679eb8006b7
Mehaa08 <99003997+Mehaa08@users.noreply.github.com>
2024-08-07 22:36:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,6 @@ st.title('˚˖𓍢ִ໋🌷͙֒✧˚.🎀༘⋆Meha Machine Learning App')
 st.info('This app builds a ml models')
 
 with st.expander('Data;):
-  st.write(**'Raw Data'**)
+  st.write('**Raw Data**')
   a = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   a
