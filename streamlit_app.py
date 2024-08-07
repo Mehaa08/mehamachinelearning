455ebac3c404bed6173b00fb18f36e4f583333b1
Mehaa08 <99003997+Mehaa08@users.noreply.github.com>
2024-08-07 22:48:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,14 @@ with st.expander('Data'):
   st.write(a)
 
   st.write('**X**')
-  X = a.drop('species',axis = 1)
-  X
+  X_raw = df.drop('species', axis=1)
+  X_raw
 
   st.write('**y**')
-  y = a.species
-  y
-  with st.expander('Data visualization'):
+  y_raw = df.species
+  y_raw
+
+with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
 # Input features
